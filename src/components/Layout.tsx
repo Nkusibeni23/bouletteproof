e4860aef42d90f3d61b80de7f168528af912ae1f
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-10-21 11:36:18 +0200

    some change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,13 +2,19 @@
 
 import Link from "next/link";
 import { useState, useEffect } from "react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [activeLink, setActiveLink] = useState("landing-page");
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   const handleLinkClick = (link: string) => {
     setActiveLink(link);
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
   };
 
   useEffect(() => {
@@ -32,7 +38,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen">
       {/* Sidebar */}
-      <aside className="w-64 bg-gray-800 text-white flex flex-col fixed inset-y-0">
+      <aside
+        className={`bg-gray-800 text-white flex flex-col fixed inset-y-0 transform ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out w-64`}
+      >
         <div className="p-4 text-2xl font-bold mt-4">Dashboard</div>
         <nav className="flex-1 p-6 space-y-4 mt-20">
           <Link href="/landing-page">
@@ -72,7 +82,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             </span>
           </Link>
         </nav>
-        <div className="p-4">
+        <div className="p-4 relative">
           <Link href="/">
             <span
               onClick={() => handleLinkClick("logout")}
@@ -87,12 +97,16 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       </aside>
 
       {/* Main Content Area */}
-      <div className="flex-1 flex flex-col ml-64">
+      <div
+        className={`flex-1 flex flex-col ${
+          isSidebarOpen ? "ml-64" : "ml-0"
+        } transition-all duration-300`}
+      >
         {/* Top bar */}
         <header
-          className={`bg-gray-200 p-4 flex justify-between items-center fixed w-full top-0 left-64 z-10 transition-shadow duration-300 ${
-            isScrolled ? "shadow-md" : ""
-          }`}
+          className={`bg-gray-200 p-4 flex justify-between items-center fixed w-full ${
+            isSidebarOpen ? "left-64" : "left-0"
+          } z-10 transition-all duration-300 shadow-md`}
         >
           <div className="text-xl font-semibold">Dashboard</div>
         </header>
@@ -100,7 +114,24 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         {/* Scrollable content */}
         <main className="p-6 bg-gray-50 flex-1 overflow-y-auto mt-16">
           {children}
+          <button
+            onClick={toggleSidebar}
+            className="flex items-center  focus:outline-none mb-6 absolute bottom-20 left-8"
+          >
+            {isSidebarOpen ? (
+              <div className=" py-2 px-4 bg-gray-600 flex items-center space-x-2 rounded-r-full">
+                <FiEyeOff className="mr-2 text-gray-200" />
+                <span className=" text-gray-200 font-semibold">Hide</span>
+              </div>
+            ) : (
+              <div className=" py-2 px-4 bg-gray-600 flex items-center space-x-2 rounded-r-full">
+                <FiEye className="mr-2 text-gray-100" />
+                <span className=" font-semibold text-gray-100">Show</span>
+              </div>
+            )}
+          </button>
         </main>
+        {/* Toggle button */}
       </div>
     </div>
   );
